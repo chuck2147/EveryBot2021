4141119da0c59acc2a6ff5f67c35877c1e3b5960
Asher <NieffAsh000@students.wvsd.org>
2021-10-20 16:56:31 -0700

    Added joystick and buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,19 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Subsystems.ClimberSubsystem;
+import frc.robot.Subsystems.DrivetrainSubsystem;
+import frc.robot.Subsystems.IndexerSubsystem;
+import frc.robot.Subsystems.IntakeSubsystem;
+
 public class RobotContainer {
-    
+   ClimberSubsystem climber = new ClimberSubsystem();
+   IndexerSubsystem index = new IndexerSubsystem();
+   IntakeSubsystem intake = new IntakeSubsystem();
+   DrivetrainSubsystem Drivetrain = new DrivetrainSubsystem();
+   private Joystick driverJoystick = new Joystick(1);
+   private JoystickButton climberButton = new JoystickButton(driverJoystick, 1);
 }
+
